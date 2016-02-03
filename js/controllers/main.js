fc35edf06a87be9ee451def367c83b55fbc7f6db
Tim Holmes <tholmes@cs.odu.edu>
2016-02-02 21:22:09 -0500

    Added sounds and hooked them up in the controller.

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -6,6 +6,14 @@
 
 	.controller('mainCtrl', function($scope, dataManager){
 
+		var kick  = new Audio();
+		var snare = new Audio();
+		var hihat = new Audio();
+
+		kick.src  = "./assets/sounds/kick.mp3";
+		snare.src = "./assets/sounds/snare.mp3";
+		hihat.src = "./assets/sounds/hihat.mp3";
+
 		dataManager.getData('./sounds/kick.json', function(response) {
 			$scope.kicks = response.data;
 		});
@@ -18,5 +26,13 @@
 			$scope.miscs = response.data;
 		});
 
+		$scope.playSound = function(id) {
+			if(id <= 4)
+				kick.play();
+			if(id > 4 && id <= 8)
+				snare.play();
+			if(id > 8)
+				hihat.play();
+		};
 	});
 })();
