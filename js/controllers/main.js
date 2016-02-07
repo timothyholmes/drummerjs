486a89f0212058c8d9279c3622eef502966a3a9d
Tim Holmes <tholmes@cs.odu.edu>
2016-02-07 17:10:52 -0500

    Double size of grid pad.

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -7,29 +7,14 @@
 	.controller('mainCtrl', function($scope, dataManager){
 
 		var refreshInterval;
-		var beatOptions = [0, 1, 2, 3];
+		var beatOptions = [0, 1, 2, 3, 4, 5, 6, 7];
 
 		var kick  = new Audio();
 		var snare = new Audio();
 		var hihat = new Audio();
 
 		var currentBeat = 0;
-		var beatCols = ['col1', 'col2', 'col3', 'col4'];
-
-		var sound1 = {
-			"id": "3",
-			"sound": "./assets/sounds/kick.mp3",
-			"type": "kick",
-			"on": true,
-			"group": "col3"
-		};
-
-		var sound2 = {
-			"id": "3",
-			"sound": "./assets/sounds/kick.mp3",
-			"type": "snare",
-			"group": "col3"
-		};
+		var beatCols = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8'];
 
 		$scope.groupOneLight = false;
 		$scope.groupTwoLight = false;
@@ -44,6 +29,10 @@
 		var second = [];
 		var third = [];
 		var fourth = [];
+		var fifth = [];
+		var sixth = [];
+		var seventh = [];
+		var eighth = [];
 		var samples = [];
 
 		dataManager.getData('./sounds/samples.json', function(response) {
@@ -61,6 +50,10 @@
 				return e.type == 'hihat';
 			});
 
+			$scope.markers = samples.filter(function(e) {
+				return e.type == 'marker';
+			});
+
 			first = samples.filter(function(e) {
 				return e.group == 'col1';
 			});
@@ -75,6 +68,22 @@
 
 			fourth = samples.filter(function(e) {
 				return e.group == 'col4';
+			});
+
+			fifth = samples.filter(function(e) {
+				return e.group == 'col5';
+			});
+
+			sixth = samples.filter(function(e) {
+				return e.group == 'col6';
+			});
+
+			seventh = samples.filter(function(e) {
+				return e.group == 'col7';
+			});
+
+			eighth = samples.filter(function(e) {
+				return e.group == 'col8';
 			});
 		});
 
@@ -107,12 +116,22 @@
 			var y = document.getElementsByClassName(beatCols[nonCurrentBeats[0]]);
 			var w = document.getElementsByClassName(beatCols[nonCurrentBeats[1]]);
 			var z = document.getElementsByClassName(beatCols[nonCurrentBeats[2]]);
+			var a = document.getElementsByClassName(beatCols[nonCurrentBeats[3]]);
+			var b = document.getElementsByClassName(beatCols[nonCurrentBeats[4]]);
+			var c = document.getElementsByClassName(beatCols[nonCurrentBeats[5]]);
+			var d = document.getElementsByClassName(beatCols[nonCurrentBeats[6]]);
+
+			console.log(z);
 
 			for (var i = 0; i < x.length; i++) {
 			    x[i].style.backgroundColor = "#F0720C";
 			    y[i].style.backgroundColor = "#F0D50C";
 			    w[i].style.backgroundColor = "#F0D50C";
 			    z[i].style.backgroundColor = "#F0D50C";
+			    a[i].style.backgroundColor = "#F0D50C";
+			    b[i].style.backgroundColor = "#F0D50C";
+			    c[i].style.backgroundColor = "#F0D50C";
+			    d[i].style.backgroundColor = "#F0D50C";
 			};
 
 			if(currentBeat == 0)
@@ -123,15 +142,23 @@
 				arrayToSend = third;
 			if(currentBeat == 3)
 				arrayToSend = fourth;
+			if(currentBeat == 4)
+				arrayToSend = fifth;
+			if(currentBeat == 5)
+				arrayToSend = sixth;
+			if(currentBeat == 6)
+				arrayToSend = seventh;
+			if(currentBeat == 7)
+				arrayToSend = eighth;
 
 			for (var i = 0; i < arrayToSend.length; i++) {
 				$scope.playSound(arrayToSend[i]);
 			}
 
 			currentBeat++;
-			if(currentBeat >= 4) 
+			if(currentBeat >= 8) 
 				currentBeat = 0;
-		}, 500)};
+		}, 250)};
 
 		$scope.stopTimer = function(){
 
@@ -143,12 +170,20 @@
 			var y = document.getElementsByClassName(beatCols[nonCurrentBeats[0]]);
 			var w = document.getElementsByClassName(beatCols[nonCurrentBeats[1]]);
 			var z = document.getElementsByClassName(beatCols[nonCurrentBeats[2]]);
+			var a = document.getElementsByClassName(beatCols[nonCurrentBeats[3]]);
+			var b = document.getElementsByClassName(beatCols[nonCurrentBeats[4]]);
+			var c = document.getElementsByClassName(beatCols[nonCurrentBeats[5]]);
+			var d = document.getElementsByClassName(beatCols[nonCurrentBeats[6]]);
 
 			for (var i = 0; i < x.length; i++) {
 			    x[i].style.backgroundColor = "#F0D50C";
 			    y[i].style.backgroundColor = "#F0D50C";
 			    w[i].style.backgroundColor = "#F0D50C";
 			    z[i].style.backgroundColor = "#F0D50C";
+			    a[i].style.backgroundColor = "#F0D50C";
+			    b[i].style.backgroundColor = "#F0D50C";
+			    c[i].style.backgroundColor = "#F0D50C";
+			    d[i].style.backgroundColor = "#F0D50C";
 			};
 
 			currentBeat = 0;
