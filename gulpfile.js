8e2423edd9576c34010c6a730928c64643955d63
Tim Holmes <tholmes@starchase.com>
2016-03-09 16:18:18 -0500

    Started getting gulp to minify and uglify code.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
 
 gulp.task('sass', function(){
   return gulp.src('src/scss/**/*.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(sass())
     .pipe(gulp.dest('src/css'))
 });
 
 gulp.task('watch', function(){
   gulp.watch('src/scss/**/*.scss', ['sass']); 
-})
+});
+
+gulp.task('useref', function(){
+  return gulp.src('src/index.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
