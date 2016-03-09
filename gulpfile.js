bdde30d8bfe1f96a245f545d352134f2d0452395
Tim Holmes <tholmes@starchase.com>
2016-03-09 16:34:21 -0500

    Got gulp to build into the dist folder, and clean. Need to get uglify working.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
 
 gulp.task('sass', function(){
   return gulp.src('src/scss/**/*.scss')
@@ -23,3 +25,20 @@ gulp.task('useref', function(){
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
 });
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 
+    ['sass', 'useref'],
+    callback
+  )
+})
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','watch'],
+    callback
+  )
+})
