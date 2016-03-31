05a6a6a7a5ccff3fd76bd2e848d968e403ac46be
Tim Holmes <tholmes@starchase.com>
2016-03-31 10:47:36 -0400

    Added sample loop

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -8,6 +8,13 @@
 		var _this = this;
 		var context = new AudioContext;
 
+		_this.loopOne = {
+			kicks: ['col0', 'col8'],
+			snares: ['col4', 'col12'],
+			hihats: ['col0','col2','col4','col6','col8','col10','col12'],
+			openhats: ['col14']
+		}
+
 		var drumKit = {
 			kick: new Kick(context),
 			snare: new Snare(context),
@@ -114,6 +121,23 @@
 			for(var i = 0; i < _this.sampler.length; i++)
 				_this.sampler[i].on = false;
 		};
+
+		var arrayCheck = function(value, array) {
+		    return array.indexOf(value) > -1;
+		}
+
+		_this.setSampleLoop = function(sampleLoop) {
+			for(var i = 0; i < _this.sampler.length; i++) {
+				if(_this.sampler[i].type == 'kick') 
+					_this.sampler[i].on = arrayCheck(_this.sampler[i].class, sampleLoop.kicks) ? true : false;
+				else if(_this.sampler[i].type == 'snare') 
+					_this.sampler[i].on = arrayCheck(_this.sampler[i].class, sampleLoop.snares) ? true : false;
+				if(_this.sampler[i].type == 'hihat') 
+					_this.sampler[i].on = arrayCheck(_this.sampler[i].class, sampleLoop.hihats) ? true : false;
+				if(_this.sampler[i].type == 'openhat') 
+					_this.sampler[i].on = arrayCheck(_this.sampler[i].class, sampleLoop.openhats) ? true : false;
+			}
+		}
 
 		var tempoSet = function(bpm) {
 			return 60000 / (4 * bpm)
