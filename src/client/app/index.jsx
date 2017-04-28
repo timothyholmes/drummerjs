312c83015096e70d8c1519c183212fb8675b6fd9
Tim Holmes <timothy.holmes@homes.com>
2017-04-28 17:01:31 -0400

    switching machines

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import {render} from 'react-dom';
-import ComponentName from './ComponentName.jsx';
-import './style/main.css';
+import ComponentName from './components/ComponentName.jsx';
+import '../style/main.css';
 
-class App extends React.Component {
-  render () {
-    return (
-      <div>
-        <p className="test">Hello my guy!</p>
-      </div>
-    );
-  }
+class DrumMachine extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            controls: {
+                tempo: 120
+            },
+            name: 'DrummerJS'
+        }
+    }
+    render () {
+        return (
+            <div>
+                <h1>{ this.state.name }</h1>
+            </div>
+        );
+    }
 }
 
-render(<App/>, document.getElementById('app'));
+render(
+    <DrumMachine />,
+    document.getElementById('app')
+);
